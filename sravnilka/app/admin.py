4e8a2485240479f06a5f36b70bcc331aeadc2fbd
milkOSTpyt <filiptema40@gmail.com>
2020-11-04 17:47:10 +0300

    04.11 end

--- sravnilka/app/admin.py
+++ sravnilka/app/admin.py
@@ -8,7 +8,7 @@ class BooksAdmin(admin.ModelAdmin):
     list_display = ('pk', 'shop', 'title', 'author', 'price')
     list_display_links = ('pk', 'title',)
     search_fields = ('title', )
-    list_filter = ('title', 'price')
+    #list_filter = ('title', 'price')
     empty_value_display = '-'
 
 
--- sravnilka/app/tasks.py
+++ sravnilka/app/tasks.py
@@ -6,37 +6,48 @@ from .models import Books
 import re
 
 
-async def cor(url, sem):
+async def cor(url, sem, dictt):
     async with sem:
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
                 html_code = await resp.text()
         dom_tree = html.fromstring(html_code)
-        of_link = 'biblio.by'
-        title = dom_tree.xpath('//div[@class="product-name"]/a')
-        author = dom_tree.xpath('//p[@class="author"]')
-        link = dom_tree.xpath('//div[@class="product-name"]/a//@href')
-        img = dom_tree.xpath('//div[@class="images-container"]//img/@src')
-        price = dom_tree.xpath('//p[@class="special-price"]//span[@class="price"] | //span[@class="regular-price"]//span[@class="price"]')
+        of_link = dictt['of_link']
+        title = dom_tree.xpath(dictt['title'])
+        author = dom_tree.xpath(dictt['author'])
+        link = dom_tree.xpath(dictt['link'])
+        img = dom_tree.xpath(dictt['img'])
+        price = dom_tree.xpath(dictt['price'])
         for t, a, l, i, p in zip(title, author, link, img, price):
             p = float(str('.'.join(re.findall('(\d+)', p.text))))
-            print(p, type(p))
-            Books(shop=of_link, title=t.text, author=a.text, link=l, img_link=i, price=p).save()
+            if 'http' not in l:
+                l = of_link + l
+            if 'http' not in i:
+                i = of_link + i
+            try:
+                Books(shop=of_link, title=t.text, author=a.text, link=l, img_link=i, price=p).save()
+            except Exception:
+                continue
 
 
-async def main(url, quantity):
+async def main(urll, quantity, dictt):
     urls = []
     for i in range(1, (quantity + 1)):
-        urls.append(f'{url}{i}')
+        urls.append(f'{urll}{i}')
     tasks = []
-    sem = asyncio.Semaphore(value=3)
+    sem = asyncio.Semaphore(value=5)
     for url in urls:
-        task = asyncio.Task(cor(url, sem))
+        task = asyncio.Task(cor(url, sem, dictt))
         tasks.append(task)
     await asyncio.gather(*tasks)
 
 
-def start(url, q):
+def start(q, dictt):
     Books.objects.all().delete()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(url, q))
+    for urll, d in dictt.items():
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main(urll, q, d))
+
+
+def www():
+    Books.objects.all().delete()
